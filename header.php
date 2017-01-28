2af1f977655437ea716927135e5885ddac72c9e9
Austin Hampton <backseatgaming98@gmail.com>
2017-01-27 19:34:32 -0500

    School Math Project

---
+++ header.php
@@ -0,0 +1,10 @@
+<head>
+    <title>&lt;Math&gt;</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta charset="ISO-8859-1">
+    <script src="http://fred-wang.github.io/mathml.css/mspace.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu|VT323&amp;subset=greek,greek-ext" rel="stylesheet">
+</head>
---
+++ nav.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="Home.Html">
+                <h3 style="margin:5px;">&lt;Math&gt;</h3>
+            </a>
+        </div>
+        <form class="navbar-form navbar-right" action="#">
+            <div class="form-group"><input type="text" class="form-control" placeholder="Search"></div><button type="submit" class="btn btn-default"
+                data-toggle="modal" data-target="#myModal">Submit</button>
+            <div id="myModal" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Under Construction</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>Sorry but the search bar is currently under maintenance</p>
+                        </div>
+                        <div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <ul class="nav navbar-nav">
+            <li>
+                <a href="Algebra.html">
+                    <h4>Basic Algebra</h4>
+                </a>
+            </li>
+            <li>
+                <a href="probandstat.html">
+                    <h4>Probability and Statistics</h4>
+                </a>
+            </li>
+            <li>
+                <a href="physics.html">
+                    <h4>Physics</h4>
+                </a>
+            </li>
+            <li>
+                <a href="precal.html">
+                    <h4>Pre-Calculus</h4>
+                </a>
+            </li>
+            <li>
+                <a href="Geometry.html">
+                    <h4>Geometry</h4>
+                </a>
+            </li>
+            <li>
+                <a href="Characters.html">
+                    <h4>Math Characters</h4>
+                </a>
+            </li>
+        </ul>
+</nav>
